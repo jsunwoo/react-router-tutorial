40e3298a1106f5a362e151875f80f41dc4462a8f
wesop <wesop@wesopui-MacBook-Pro.local>
2018-11-02 18:22:27 +0900

    How to use basic Redirect comp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Posts from "./routes/Posts";
+import MyPage from "./routes/MyPage";
+import Login from "./routes/Login";
 import Header from "./components/Header";
 
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
         <Route exact path="/" component={Home} />
         <Route path="/about/:username" component={About} />
         <Route path="/posts" component={Posts} />
+        <Route path="/login" component={Login} />
+        <Route path="/me" component={MyPage} />
       </div>
     </Router>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,19 @@ const Header = () => {
   return (
     <div className="header">
       <NavLink exact to="/" className="item">
-        a
+        홈
       </NavLink>
       <NavLink to="/about/velopert" className="item">
-        b
+        소개
       </NavLink>
       <NavLink to="/posts" className="item">
-        c
+        포스트
+      </NavLink>
+      <NavLink to="/me" className="item">
+        마이페이지
+      </NavLink>
+      <NavLink to="/Login" className="item">
+        로그인
       </NavLink>
     </div>
   );
