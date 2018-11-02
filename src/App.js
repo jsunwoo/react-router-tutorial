6f61bb6361e59a6f1305cac41a7e0dc074289d5a
wesop <wesop@wesopui-MacBook-Pro.local>
2018-11-02 15:12:19 +0900

    Add Route comp for About.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import About from "./routes/About";
 
 const App = () => {
   return (
     <Router>
       <div>
         <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
       </div>
     </Router>
   );
