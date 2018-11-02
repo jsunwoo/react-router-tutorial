94ac1f8ef0087b3a6f43fe28fcbd505d86605898
wesop <wesop@wesopui-MacBook-Pro.local>
2018-11-02 15:12:58 +0900

    How to use exact path

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <Router>
       <div>
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
       </div>
     </Router>
